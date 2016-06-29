8ca480d80ad19ef3e96073bbd212e6c4bd987d02
ligun <choonggeun@gmail.com>
2016-06-30 01:49:27 +0900

    change page controller authentication

--- grails-app/controllers/gingham/PageController.groovy
+++ grails-app/controllers/gingham/PageController.groovy
@@ -4,10 +4,12 @@ import grails.plugin.springsecurity.annotation.Secured
 
 @Secured(['IS_AUTHENTICATED_REMEMBERED'])
 class PageController {
+    @Secured(['IS_AUTHENTICATED_ANONYMOUSLY'])
     def index() {
         [pageList: Page.list()]
     }
 
+    @Secured(['IS_AUTHENTICATED_ANONYMOUSLY'])
     def show(Page page) {
         def html = Marked.marked(page.markdown)
         [id: page.id, title: page.title, html: html]
